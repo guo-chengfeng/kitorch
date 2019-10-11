14a24bb775da31b26e46a934721f07f3b94f3c65
fiberkzx <156690308@qq.com>
2019-10-11 11:24:31 +0800

    add rnn

--- nn/__init__.py
+++ nn/__init__.py
@@ -3,7 +3,7 @@ from .conv2d import Conv2d
 from .linear import Linear
 from .dropout import Dropout, Dropout2d
 from .module import Module
-# from .rnn_manual import RNN,LSTM
+from .rnn import RNN,LSTM
 from .batchnorm import BatchNorm, BatchNorm2d, BatchNorm1d
 
-__all__ = ['BatchNorm', 'BatchNorm2d', 'BatchNorm1d', 'Dropout', 'Dropout2d', 'Conv2d', 'Linear', 'Module', 'Layer']
+__all__ = ['RNN','LSTM','BatchNorm', 'BatchNorm2d', 'BatchNorm1d', 'Dropout', 'Dropout2d', 'Conv2d', 'Linear', 'Module', 'Layer']
